67be554826022e45f1080370e3d6b1cd976821e9
murkrow <marcocoppola02.mc@gmail.com>
2024-07-03 11:13:35 +0200

    Basic stuff working

--- lib/features/widgets/presentation/states/installed_widgets_state.dart
+++ lib/features/widgets/presentation/states/installed_widgets_state.dart
@@ -2,13 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:mosaico/shared/states/loadable_state.dart';
 import 'package:mosaico_flutter_core/features/mosaico_loading/presentation/states/mosaico_loading_state.dart';
 import 'package:mosaico_flutter_core/features/mosaico_widgets/data/models/mosaico_widget.dart';
+import 'package:mosaico_flutter_core/features/mosaico_widgets/data/models/mosaico_widget_configuration.dart';
+import 'package:mosaico_flutter_core/features/mosaico_widgets/data/repositories/mosaico_widget_configurations_repository_impl.dart';
 import 'package:mosaico_flutter_core/features/mosaico_widgets/data/repositories/mosaico_widgets_repository_impl.dart';
+import 'package:mosaico_flutter_core/features/mosaico_widgets/domain/repositories/mosaico_widget_configurations_repository.dart';
 import 'package:mosaico_flutter_core/features/mosaico_widgets/domain/repositories/mosaico_widgets_repository.dart';
 
 class InstalledWidgetsState extends LoadableState {
-  /// Widget repository
+
+  /// Repositories
   final MosaicoWidgetsRepository _widgetsRepository =
       MosaicoWidgetsRepositoryImpl();
+  final MosaicoWidgetConfigurationsRepository _configurationsRepository =
+      MosaicoWidgetConfigurationsRepositoryImpl();
 
   /// List of installed widgets
   List<MosaicoWidget>? _widgets;
@@ -25,6 +31,28 @@ class InstalledWidgetsState extends LoadableState {
     loadingState.showLoading();
     _widgets = await _widgetsRepository.getInstalledWidgets();
     loadingState.hideLoading();
+    notifyListeners();
+  }
+
+  Future<void> deleteWidget(MosaicoWidget widget) async {
+    loadingState.showOverlayLoading();
+    await _widgetsRepository.uninstallWidget(widgetId: widget.id);
+    _widgets?.remove(widget);
+    loadingState.hideOverlayLoading();
+    notifyListeners();
+  }
+
+  Future<List<MosaicoWidgetConfiguration>> getWidgetConfigurations(MosaicoWidget widget) async {
+    loadingState.showOverlayLoading();
+    final configurations = await _configurationsRepository.getWidgetConfigurations(widgetId: widget.id);
+    loadingState.hideOverlayLoading();
+    return configurations;
+  }
+
+  Future<void> previewWidget(MosaicoWidget widget, {MosaicoWidgetConfiguration? configuration}) async {
+    loadingState.showOverlayLoading();
+    await _widgetsRepository.previewWidget(widgetId: widget.id, configurationId: configuration?.id);
+    loadingState.hideOverlayLoading();
     notifyListeners();
   }
 }
