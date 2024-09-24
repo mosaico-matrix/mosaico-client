3fef0b6c81cf2231524bd5e6c52574f75b9421c8
Murkrow <marcocoppola02.mc@gmail.com>
2024-09-24 23:21:26 +0200

    Maiz

--- lib/features/slideshows/presentation/widgets/slideshow_editor_item_card/slideshow_editor_item_card_duration.dart
+++ lib/features/slideshows/presentation/widgets/slideshow_editor_item_card/slideshow_editor_item_card_duration.dart
@@ -11,22 +11,20 @@ class SlideshowEditorItemCardDuration extends StatelessWidget {
     _durationController.text = initialDuration?.toString() ?? '';
     return Padding(
       padding: const EdgeInsets.only(top: 10.0),
-      child: Expanded(
-        child: TextFormField(
-          controller: _durationController,
-          cursorColor: Theme.of(context).colorScheme.onPrimary,
-          keyboardType: TextInputType.number,
-          decoration:  InputDecoration(
-            errorText: checkDuration(),
-            fillColor: Colors.white,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            filled: true,
-            hintText: 'Seconds',
+      child: TextFormField(
+        controller: _durationController,
+        cursorColor: Theme.of(context).colorScheme.onPrimary,
+        keyboardType: TextInputType.number,
+        decoration:  InputDecoration(
+          errorText: checkDuration(),
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
-          onChanged: (value) => value != '' ? onDurationChanged(int.parse(value)) : null,
+          filled: true,
+          hintText: 'Seconds',
         ),
+        onChanged: (value) => value != '' ? onDurationChanged(int.parse(value)) : null,
       ),
     );
   }
